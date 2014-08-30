e6bcca9eb253b14cf1b0fd25de240e47df7ee5d1
Kevin Wu <kevinwu.business@gmail.com>
2014-08-30 00:16:11 -0700

    Added basic HeimerFile class.

---
+++ heimer/util.py
@@ -0,0 +1,27 @@
+""" General utility class used by the various components. """
+
+class HeimerFile:
+    """ Simple custom file class used by code generation components. """
+
+    indentString = "    "
+
+    def __init__ ( self, name ):
+        self.internalFile = open ( name, 'w' )
+        self.indentLevel = 0
+
+    def indent():
+        self.indentLevel += 1
+
+    def dedent():
+        self.indentLevel -= 1
+
+    def write (line):
+        self.internalFile.write (HeimerFile.indentString * self.indentLevel + line)
+
+    def writeLine (line):
+        self.write (line + "\n")
+
+    def close():
+        self.internalFile.close()
+
+
