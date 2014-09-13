328a5d668fda4bf024043cac0d7d0a1edac146b8
Kevin Wu <kevinwu.business@gmail.com>
2014-09-13 16:39:24 -0700

    Reorganized up CodeGenerator in codegen.py.

--- src/codegen.py
+++ src/codegen.py
@@ -36,17 +36,61 @@ class CodeGenerator:
         """ Perform additional initialization if required. """
         pass
 
+    def codeGen(self):
+        """ This method is called to generate and write the parser to the specified file. """
+        self.generateDataFile()
+        self.generateUtilFile()
+        self.generateMainFile()
+        self.output.save()
+        self.util.save()
+        self.data.save()
+
+    ################################################################################
+    # Generate Data File
+    ################################################################################
+
     def generateDataFile(self):
         """ Generate classes in a separate data file. """
         self.currentFile = self.data
         self.generateDataFileHeader()
         self.generateClasses()
 
+    def generateDataFileHeader(self):
+        """ For generating the data file header, such as the import statements. """
+        raise NotImplementedError()
+
+    def generateClasses(self):
+        """ For generating code segment that defines all the data structures needed by the parser. """
+        for className, fieldNamesAndTypes in self.format.classes():
+            self.generateClass( className, fieldNamesAndTypes )
+
+    def generateClass( self, className, fieldNamesAndTypes ):
+        """ Helper function for generating the code segement defining a class (or the corresponding
+        data structure). The first argument is the class name and the second argument is a dictionary
+        containing key-value pairs of the form (field name, field type), both as strings. """
+        raise NotImplementedError()
+
+    ################################################################################
+    # Generate Util File
+    ################################################################################
+
     def generateUtilFile(self):
         """ Generate helper functions in the separate util file. """
         self.currentFile = self.util
         self.generateUtilFileHeader()
         self.generateHelperFunctions()
+
+    def generateUtilFileHeader(self):
+        """ For generating the util file header, such as the import statements. """
+        raise NotImplementedError()
+
+    def generateHelperFunctions(self):
+        """ For generating the helper functions that will be useful when parsing in the util file. """
+        raise NotImplementedError()
+
+    ################################################################################
+    # Generate Main File
+    ################################################################################
 
     def generateMainFile(self):
         """ Generate main file where the main function resides. """
@@ -58,25 +102,8 @@ class CodeGenerator:
         self.generateRunFunction()
         self.generateMainFunction()
 
-    def generateDataFileHeader(self):
-        """ For generating the data file header, such as the import statements. """
-        raise NotImplementedError()
-
-    def generateUtilFileHeader(self):
-        """ For generating the util file header, such as the import statements. """
-        raise NotImplementedError()
-
     def generateMainFileHeader(self):
         """ For generating the main file header, such as the import statements. """
-        raise NotImplementedError()
-
-    def generateClasses(self):
-        """ For generating code segment that defines all the data structures needed by the parser. """
-        for className, fieldNamesAndTypes in self.format.classes():
-            self.generateClass( className, fieldNamesAndTypes )
-
-    def generateHelperFunctions(self):
-        """ For generating the helper functions that will be useful when parsing in the util file. """
         raise NotImplementedError()
 
     def generateOptionVariables(self):
@@ -98,18 +125,3 @@ class CodeGenerator:
     def generateMainFunction(self):
         """ For generating the empty main method that the user can fill in. """
         raise NotImplementedError()
-
-    def generateClass( self, className, fieldNamesAndTypes ):
-        """ Helper function for generating the code segement defining a class (or the corresponding
-        data structure). The first argument is the class name and the second argument is a dictionary
-        containing key-value pairs of the form (field name, field type), both as strings. """
-        raise NotImplementedError()
-
-    def codeGen(self):
-        """ This method is called to generate and write the parser to the specified file. """
-        self.generateDataFile()
-        self.generateUtilFile()
-        self.generateMainFile()
-        self.output.save()
-        self.util.save()
-        self.data.save()
