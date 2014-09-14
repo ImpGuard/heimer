8f76623736c3b604e63821f49c9b02b70f5c9e16
Kevin Wu <kevinwu.business@gmail.com>
2014-09-13 17:51:32 -0700

    Refactored JavaGenerator to use new CodeGenerator template.

--- src/javagen.py
+++ src/javagen.py
@@ -2,44 +2,102 @@ from codegen import CodeGenerator
 from parser import StringConstants
 from converter import *
 from optparse import OptionParser
+import os
 
 """ Class for generating Java code. """
 class JavaGenerator(CodeGenerator):
 
-    def generateFileHeader(self):
-        """ For generating the file header, such as the import statements. """
-        # Import library headers
-        self.output.writeLine("import java.util.ArrayList;")
-        # Import class files
-        # for (className, fields) in self.format.classes():
-        #     self.output.writeLine("import " + className + ";")
-        self.output.writeNewline()
+    def initialize(self):
+        """ Perform additional initialization if required. """
+        self.output.setExtension("java")
+        self.util.setExtension("java")
+        self.data.setExtension("java")
+
+    ################################################################################
+    # Generate Data File
+    ################################################################################
+
+    def generateDataFileHeader(self):
+        """ For generating the data file header, such as the import statements. """
+        pass
+
+    def generateClass( self, className, fieldNamesAndTypes ):
+        """ Helper function for generating the code segement defining a class (or the corresponding
+        data structure). The first argument is the class name and the second argument is a dictionary
+        containing key-value pairs of the form (field name, field type), both as strings. """
+        pass
+
+    ################################################################################
+    # Generate Util File
+    ################################################################################
+
+    def generateUtilFile(self):
+        """ Generate helper functions in the separate util file. """
+        self.currentFile = self.util
+        self.generateUtilFileHeader()
+        self.generateHelperFunctions()
+
+    def generateUtilFileHeader(self):
+        """ For generating the util file header, such as the import statements. """
+        pass
 
     def generateHelperFunctions(self):
-        """ Generate any helper functions that will be useful when parsing. """
-        # Convert value to bool
+        """ For generating the helper functions that will be useful when parsing in the util file. """
+        self._beginBlock("public class " + CodeGenerator.UTIL_FILE_NAME)
+
+        # converToBool
         self._beginBlock("public static boolean convertToBool(String name)")
         self._beginBlock("if (name.equals(\"1\") || name.toLowerCase().equals(\"true\"))")
-        self.output.writeLine("return true;")
+        self.currentFile.writeLine("return true;")
         self._endBlock()
         self._beginBlock("else if (name.equals(\"0\") || name.toLowerCase().equals(\"false\"))")
-        self.output.writeLine("return false;")
+        self.currentFile.writeLine("return false;")
         self._endBlock()
-        self.output.writeLine("throw new NumberFormatException();")
+        self.currentFile.writeLine("throw new NumberFormatException();")
         self._endBlock()
-        self.output.writeNewline()
+        self.currentFile.writeNewline()
+
+        self._endBlock()
+
+    ################################################################################
+    # Generate Main File
+    ################################################################################
+
+    def generateMainFile(self):
+        """ Generate main file where the main function resides. """
+        self.currentFile = self.output
+        self.generateMainFileHeader()
+        self._beginBlock("public class " + os.path.splitext(os.path.basename(self.currentFile.filename))[0])
+        self.generateMainFunction()
+        self.generateRunFunction()
+        self.generateOptionVariables()
+        self.generateOptionParserFunction()
+        self.generateInputParserFunction()
+        self._endBlock()
+
+    def generateMainFileHeader(self):
+        """ For generating the main file header, such as the import statements. """
+        # Import library headers
+        self.currentFile.writeLine("import java.util.ArrayList;")
+        # Import Heimer Files
+        self.currentFile.writeLine("import " + CodeGenerator.DATA_FILE_NAME + ";")
+        self.currentFile.writeLine("import " + CodeGenerator.UTIL_FILE_NAME + ";")
+        # Import class files in currentFile
+        for (className, fields) in self.format.classes():
+            self.currentFile.writeLine("import " + className + ";")
+        self.currentFile.writeNewline()
 
     def generateOptionVariables(self):
         """ Generate global option variables that will be initialized when parsing. """
-        self.output.writeLine("public static ArrayList<String> userArgs = new ArrayList<String>();")
+        self.currentFile.writeLine("public static ArrayList<String> "+ CodeGenerator.USER_ARGS +" = new ArrayList<String>();")
 
         if len(self.format.commandLineOptions()) == 0:
             return
 
         options = self.format.commandLineOptions()
         for option in options:
-            self.output.writeLine("public static " + self._getTypeName(option.optionType) + " " + option.variableName + ";")
-        self.output.writeNewline()
+            self.currentFile.writeLine("public static " + self._getTypeName(option.optionType) + " " + option.variableName + ";")
+        self.currentFile.writeNewline()
 
     def generateHelpMessage(self):
         # Create option parser and generate helpMessage strings
@@ -61,23 +119,23 @@ class JavaGenerator(CodeGenerator):
             helpMessage.append(helpString)
 
         # Generate the help message multiline string
-        self.output.write("String USAGE = \"" + helpMessage[0] + "\\n\"")
+        self.currentFile.write("String USAGE = \"" + helpMessage[0] + "\\n\"")
         for index, helpString in enumerate(helpMessage[1:]):
-            self.output.writeNewline()
+            self.currentFile.writeNewline()
             if index == 0:
-                self.output.indent()
-            self.output.write(" + \"" + helpString + "\\n\"")
-        self.output.writeLine(";")
+                self.currentFile.indent()
+            self.currentFile.write(" + \"" + helpString + "\\n\"")
+        self.currentFile.writeLine(";")
 
         if len(helpMessage) > 1:
-            self.output.dedent()
+            self.currentFile.dedent()
 
     def generateOptionParserFunction(self):
         """ For generating the function to parse command line options. """
         if len(self.format.commandLineOptions()) == 0:
             return
 
-        writeLine = self.output.writeLine
+        writeLine = self.currentFile.writeLine
         options = self.format.commandLineOptions()
 
         # Create helper for code generation
@@ -91,7 +149,7 @@ class JavaGenerator(CodeGenerator):
             self._endBlock()
 
         # Create option parser function
-        self._beginBlock("private static void parseOptions(String[] args)")
+        self._beginBlock("private static void " + CodeGenerator.PARSE_OPTIONS + "(String[] args)")
         self.generateHelpMessage()
         self._beginBlock("try")
         self._beginBlock("for ( int i = 0; i < args.length; i++ )")
@@ -102,7 +160,7 @@ class JavaGenerator(CodeGenerator):
 
         # generate code for handling extraneous inputs
         self._beginBlock("else")
-        writeLine("userArgs.add(args[i]);")
+        writeLine(CodeGenerator.USER_ARGS + ".add(args[i]);")
         writeLine("i += 1;")
         self._endBlock()
 
@@ -118,65 +176,36 @@ class JavaGenerator(CodeGenerator):
         self._endBlock()
 
         # generate code for returning the filename
-        self._beginBlock("if (userArgs.size() == 0)")
+        self._beginBlock("if (" + CodeGenerator.USER_ARGS + ".size() == 0)")
         writeLine("System.out.println(USAGE);")
         writeLine("System.exit(1);")
         self._endBlock()
 
         # End of function
         self._endBlock()
-        self.output.writeNewline()
+        self.currentFile.writeNewline()
 
     def generateInputParserFunction(self):
         """ For generating the function to parse an input file. """
-        pass # FIXME
+        pass
 
     def generateRunFunction(self):
         """ For generating the function that will be called by the user. """
-        self._beginBlock("public static void run(String[] args)")
-        self.output.writeLine("parseOptions(args);")
+        self._beginBlock("public static void " + CodeGenerator.RUN + "(String[] args)")
+        self.output.writeLine(CodeGenerator.PARSE_OPTIONS + "(args);")
         self._endBlock()
         self.output.writeNewline()
 
-    def generateMain(self):
+    def generateMainFunction(self):
         """ For generating the empty main method that the user can fill in. """
         self._beginBlock("public static void main(String[] args)")
-        self.output.writeLine("run(args);")
+        self.output.writeLine(CodeGenerator.RUN + "(args);")
         self._endBlock()
+        self.output.writeNewline()
 
-
-    def generateClass( self, className, fieldNamesAndTypes ):
-        """ Helper function for generating the code segement defining a class (or the corresponding
-        data structure). The first argument is the class name and the second argument is a dictionary
-        containing key-value pairs of the form (field name, field type), both as strings. """
-        pass # FIXME
-
-    def generateMainClass(self):
-        """ Helper to generate the main class that wraps the parsers. """
-        self.generateFileHeader()
-        self._beginBlock("public class Main")
-        self.generateOptionVariables()
-        self.generateHelperFunctions()
-        self.generateOptionParserFunction()
-        self.generateInputParserFunction()
-        self.generateRunFunction()
-        self.generateMain()
-        self._endBlock()
-
-    def codeGen(self):
-        """ This method is called to generate and write the parser to the specified file. """
-        self.generateClasses()
-        self.generateMainClass()
-        self.output.save()
-
-    def _beginBlock( self, line ):
-        self.output.writeLine(line)
-        self.output.writeLine("{")
-        self.output.indent()
-
-    def _endBlock(self):
-        self.output.dedent()
-        self.output.writeLine("}")
+    ################################################################################
+    # Helper Functions
+    ################################################################################
 
     def _getTypeName( self, typeName ):
         if isInteger(typeName):
@@ -188,3 +217,11 @@ class JavaGenerator(CodeGenerator):
         elif isList(typeName):
             return "ArrayList<" + _getTypeName(listType(typeName)) + ">"
 
+    def _beginBlock( self, line ):
+        self.currentFile.writeLine(line)
+        self.currentFile.writeLine("{")
+        self.currentFile.indent()
+
+    def _endBlock(self):
+        self.currentFile.dedent()
+        self.currentFile.writeLine("}")
