3c01d6446321dca7bd3e02c055bd1296616035aa
Leo Kam <leohtkam@gmail.com>
2014-10-04 17:18:21 -0700

    Add some failure end-to-end tests

--- test/pygen_test.py
+++ test/pygen_test.py
@@ -5,10 +5,12 @@ import sys, os
 
 tests = [
     getTestByName(0, "image", "py"),
-    getTest(0, "Vector.format", "Vector_pass.py", "Vector_pass1.input", "Vector_pass1.sln"),
-    getTest(0, "Vector.format", "Vector_pass.py", "Vector_pass2.input", "Vector_pass2.sln"),
-    getTest(0, "Vector.format", "Vector_pass.py", "Vector_pass3.input", "Vector_pass3.sln"),
-    getTest(0, "Vector.format", "Vector_pass.py", "Vector_pass4.input", "Vector_pass4.sln")
+    getTest(0, "Vector.format", "Vector.py", "Vector_pass1.input", "Vector_pass1.sln"),
+    getTest(0, "Vector.format", "Vector.py", "Vector_pass2.input", "Vector_pass2.sln"),
+    getTest(0, "Vector.format", "Vector.py", "Vector_pass3.input", "Vector_pass3.sln"),
+    getTest(0, "Vector.format", "Vector.py", "Vector_pass4.input", "Vector_pass4.sln"),
+    (4, "test/tests/pass/Vector.format", "test/tests/pass/Vector.py", "test/tests/fail/Vector_fail1.input", "test/tests/fail/Vector_fail1.sln"),
+    (4, "test/tests/pass/Vector.format", "test/tests/pass/Vector.py", "test/tests/fail/Vector_fail2.input", "test/tests/fail/Vector_fail2.sln")
 ]
 
 def testPyGen():
